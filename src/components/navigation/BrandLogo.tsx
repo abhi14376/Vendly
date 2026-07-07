05f84a66e2b734233ed1d1ae2232f67cfbad7e6a
Abhishek <abhi@example.com>
2026-07-07 16:06:57 +0530

    style: double the logo size again to 6x

--- src/components/navigation/BrandLogo.tsx
+++ src/components/navigation/BrandLogo.tsx
@@ -9,7 +9,7 @@ export function BrandLogo({ compact = false, inverse = false }: BrandLogoProps) 
       <img 
         src="/logo.png" 
         alt="BidTracker" 
-        className={`h-10 w-auto object-contain scale-[3] origin-left ${compact ? 'w-10 object-left' : ''} ${inverse ? 'brightness-0 invert' : ''}`}
+        className={`h-10 w-auto object-contain scale-[6] origin-left ${compact ? 'w-10 object-left' : ''} ${inverse ? 'brightness-0 invert' : ''}`}
         style={compact ? { objectPosition: 'left', width: '40px', objectFit: 'cover' } : {}}
       />
     </span>
